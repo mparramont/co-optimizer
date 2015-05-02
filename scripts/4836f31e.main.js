c4ca48a2671ab88fd191721013f76b713b41cb93
Miguel Parramon <miguel@ztory.com>
2015-05-02 17:18:02 +0200

    Fixes?

---
+++ scripts/4836f31e.main.js
@@ -0,0 +1,1 @@
+var suggestions=$(".game-suggestions tr"),suggestion=$(_.sample(suggestions)),id=suggestion.data("id"),title=suggestion.find("td:first-child").text(),genre=suggestion.find("td:last-child").text();$(".game-title").text(title),$(".game-genre").text(genre),$(".game-image").attr("src","http://www.co-optimus.com/image.php?id="+id);
