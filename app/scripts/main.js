0d07640f9a964748fb0ef2246bee8063c441b4cc
Miguel Parramon <miguel@ztory.com>
2015-05-02 13:36:50 +0200

    Suggestions work!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,8 @@
-console.log('\'Allo \'Allo!');
+var suggestions = $('.game-suggestions tr');
+var suggestion = $(_.sample(suggestions));
+var id = suggestion.data('id');
+var title = suggestion.find('td:first-child').text();
+var genre = suggestion.find('td:last-child').text();
+$('.game-title').text(title);
+$('.game-genre').text(genre);
+$('.game-image').attr('src', 'http://www.co-optimus.com/image.php?id=' + id);
